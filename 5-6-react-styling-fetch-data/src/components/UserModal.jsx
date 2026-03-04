d2bc321d14a89a8ede23b5076ebce7aa71d80f37
batool3-A <s202160170@kfupm.edu.sa>
2026-03-04 11:46:36 +0300

    TODO 2&3 done

--- 5-6-react-styling-fetch-data/src/components/UserModal.jsx
+++ 5-6-react-styling-fetch-data/src/components/UserModal.jsx
@@ -10,6 +10,29 @@ function UserModal({ show, user, onHide }) {
       {/* TODO 3.2: Modal must use show={show} and onHide={onHide} */}
       {/* TODO 3.2: Must include Header/Title/Body/Footer + Close button */}
       {/* TODO 3.2: Body must show avatar (user-avatar-large) + name/email/phone/website */}
+      <Modal show={show} onHide={onHide}>
+              <Modal.Header closeButton>
+                <Modal.Title>{user.name}</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body className="text-center">
+                <img
+                  src={`https://i.pravatar.cc/150?img=${user.id}`}
+                  alt={user.name}
+                  className="user-avatar-large mb-3"
+                />
+
+                <p><strong>Email:</strong> {user.email}</p>
+                <p><strong>Phone:</strong> {user.phone}</p>
+                <p><strong>Website:</strong> {user.website}</p>
+              </Modal.Body>
+
+              <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
     </>
   );
 }
